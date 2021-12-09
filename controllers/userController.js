12b5cea877ebdbd91ff4f96490806a4776ac7e4f
farizaiis <farizaiis@hotmail.com>
2021-12-09 14:27:58 +0700

    set CRUD user

---
+++ controllers/userController.js
@@ -0,0 +1,247 @@
+const { user } = require('../models');
+const Joi = require('joi');
+
+module.exports = {
+    register: async (req, res) => {
+        const body = req.body;
+
+        try {
+            const schema = Joi.object({
+                name: Joi.string().required(),
+                age: Joi.number().required().min(1).max(100),
+            });
+
+            const check = schema.validate(
+                {
+                    name: body.name,
+                    age: body.age,
+                },
+                { abortEarly: false }
+            );
+
+            if (check.error) {
+                return res.status(400).json({
+                    status: 'failed',
+                    message: 'bad request',
+                    errors: check.error['details'].map(
+                        ({ message }) => message
+                    ),
+                });
+            }
+
+            const checkName = await user.findOne({
+                where: { name: body.name, age: body.age },
+            });
+
+            if (checkName) {
+                return res.status(400).json({
+                    status: 'failed',
+                    message: 'Cannot duplicate data',
+                });
+            }
+
+            await user.create({
+                name,
+                age,
+            });
+
+            return res.status(200).json({
+                status: 'success',
+                message: 'Successfully registered a new user',
+            });
+        } catch (error) {
+            return res.status(500).json({
+                status: 'failed',
+                message: 'internal server error',
+            });
+        }
+    },
+
+    getAll: async (req, res) => {
+        try {
+            const findData = await user.findAll({
+                attributes: { exclude: [age, updateAt] },
+            });
+
+            const checkData = await user.findAndCountAll();
+
+            if (checkData < 1) {
+                return res.status(200).json({
+                    status: 'success',
+                    message:
+                        'There is no user data, please register for new user',
+                });
+            }
+
+            return res.status(200).json({
+                status: 'success',
+                message: 'Success retrieve user data',
+                data: findData,
+            });
+        } catch (error) {
+            return res.status(500).json({
+                status: 'failed',
+                message: 'internal server error',
+            });
+        }
+    },
+
+    getOne: async (req, res) => {
+        const id = req.params.id;
+        try {
+            const findData = await user.findOne({
+                where: { id },
+            });
+
+            if (!findData) {
+                return res.status(400).json({
+                    status: 'failed',
+                    message: 'Data not found',
+                });
+            }
+
+            return res.status(200).json({
+                status: 'success',
+                message: 'Success retrieve user data',
+                data: findData,
+            });
+        } catch (error) {
+            return res.status(500).json({
+                status: 'failed',
+                message: 'internal server error',
+            });
+        }
+    },
+
+    update: async (req, res) => {
+        const id = req.params.id;
+        const body = req.body;
+        try {
+            const schema = Joi.object({
+                name: Joi.string().required(),
+                age: Joi.number().required().min(1).max(100),
+            });
+
+            const check = schema.validate(
+                {
+                    name: body.name,
+                    age: body.age,
+                },
+                { abortEarly: false }
+            );
+
+            if (check.error) {
+                return res.status(400).json({
+                    status: 'failed',
+                    message: 'bad request',
+                    errors: check.error['details'].map(
+                        ({ message }) => message
+                    ),
+                });
+            }
+
+            const findData = await user.findOne({
+                where: { id },
+            });
+
+            if (!findData) {
+                return res.status(400).json({
+                    status: 'failed',
+                    message: 'Data not found',
+                });
+            }
+
+            if (body.name && body.age) {
+                const checkData = await user.findOne({
+                    where: { name: body.name, age: body.age },
+                });
+
+                if (checkData) {
+                    return res.status(400).json({
+                        status: 'failed',
+                        message: 'Cannot duplicate data',
+                    });
+                }
+            }
+
+            if (body.name) {
+                const checkData = await user.findOne({
+                    where: { name: body.name, age: findData.dataValues.age },
+                });
+
+                if (checkData) {
+                    return res.status(400).json({
+                        status: 'failed',
+                        message: 'Cannot duplicate data',
+                    });
+                }
+            }
+
+            if (body.age) {
+                const checkData = await user.findOne({
+                    where: { name: findData.dataValues.name, age: body.age },
+                });
+
+                if (checkData) {
+                    return res.status(400).json({
+                        status: 'failed',
+                        message: 'Cannot duplicate data',
+                    });
+                }
+            }
+
+            await user.update(
+                {
+                    name: body.name,
+                    age: body.age,
+                },
+                {
+                    where: {
+                        id,
+                    },
+                }
+            );
+
+            return res.status(200).json({
+                status: 'success',
+                message: 'Success updated a user',
+            });
+        } catch (error) {
+            return res.status(500).json({
+                status: 'failed',
+                message: 'internal server error',
+            });
+        }
+    },
+
+    delete: async (req, res) => {
+        const id = req.params.id;
+        try {
+            const findData = await user.findOne({
+                where: { id },
+            });
+
+            if (!findData) {
+                return res.status(400).json({
+                    status: 'failed',
+                    message: 'Data not found',
+                });
+            }
+
+            await user.destroy({
+                where: { id },
+            });
+
+            return res.status(200).json({
+                status: 'success',
+                message: 'Success deleted the data',
+                data: findData,
+            });
+        } catch (error) {
+            return res.status(500).json({
+                status: 'failed',
+                message: 'internal server error',
+            });
+        }
+    },
+};
--- migrations/20211209070036-create-user.js
+++ migrations/20211209070036-create-user.js
@@ -1,30 +1,32 @@
 'use strict';
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('users', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      name: {
-        type: Sequelize.STRING
-      },
-      age: {
-        type: Sequelize.INTEGER
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('users');
-  }
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.createTable('users', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER,
+            },
+            name: {
+                allowNull: false,
+                type: Sequelize.STRING,
+            },
+            age: {
+                allowNull: false,
+                type: Sequelize.INTEGER,
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+        });
+    },
+    down: async (queryInterface, Sequelize) => {
+        await queryInterface.dropTable('users');
+    },
 };
